c2cad7a8502e455b38f5d269cc1f19cb7590b473
Muhammad Touseef <touseefrajput2008@gmail.com>
2026-07-12 12:54:33 -0700

    Add Python program to find the ASCII value of a character

---
+++ 24_asci_value.py
@@ -0,0 +1,8 @@
+"""
+Program: Find the ASCII Value of a Character
+Description: Takes a character as input and displays its ASCII value.
+"""
+
+character = input("Enter a character: ")
+
+print(f"The ASCII value of '{character}' is {ord(character)}")
