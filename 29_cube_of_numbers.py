bff01d8eb75ee03e1cbc6e8dac37a3b075e7d888
Muhammad Touseef <touseefrajput2008@gmail.com>
2026-07-17 13:02:24 -0700

    Add Python program to calculate the cube of a number

---
+++ 29_cube_of_numbers.py
@@ -0,0 +1,10 @@
+# Program: Calculate the cube of a number
+
+# Take input from the user
+num = int(input("Enter a number: "))
+
+# Calculate the cube
+cube = num ** 3
+
+# Display the result
+print(f"The cube of {num} is {cube}")
